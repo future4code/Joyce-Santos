b197e282940c668b47e675915fb9c766cab7ed79
Joyce Roberta <joycinha0406@gmail.com>
2020-09-28 15:36:51 -0300

    resolução-dos-exercicios-propostos-js

---
+++ semana3/variaveis/js-vanilla-template/src/index.js
@@ -0,0 +1,113 @@
+a = 10
+b = 10
+
+console.log(b)
+
+b = 5
+console.log(a, b)
+
+/* Exercicio de interpretação 1 - 
+10
+10 5
+*/ 
+
+a = 10
+b = 20
+c = a
+b = c
+a = b
+
+console.log(a, b, c)
+
+/* Exercicio de interpretação 2 -
+
+10
+10
+10
+*/ 
+
+// Exercício de escrita de código 1 - a)
+
+/*let nome
+
+// Exercício de escrita de código 1 - b)
+
+let idade
+
+// 1 - c)
+
+console.log(typeof nome)
+console.log(typeof idade)
+
+/* Exercício de escrita de código 1 - d)
+Foi impresso "undefined" pois não fora atribuido nenhum valor as variáveis. */
+
+// Exercício de escrita de código 1 - e)
+
+/* prompt("Digite o seu nome")
+prompt("Digite a sua idade")
+
+console.log(nome)
+console.log(idade)
+
+console.log(typeof nome)
+console.log(typeof idade)
+
+/* Apareceu uma caixinha de texto solicitando o nome eposteriormente uma outra caixinha de texto solicitando a idade.*/
+
+// Exercício de escrita de código 1 - f)
+
+/*let nome= prompt('Qual seu nome?');
+console.log(nome);
+let idade= prompt("qual sua idade?");
+console.log( idade);
+// O retorno foi uma "string" para o nome e um number para a idade.
+console.log(`Olá ${nome}, você tem ${idade} anos`);*/
+
+// Exercício de escrita de código 2 -
+
+/*let generoLiterario = prompt("Qual seu gênero literário favorito?")
+console.log(`Resposta: ${generoLiterario}`)
+
+let autor
+autor = prompt("Qual seu autor favorito?")
+console.log(`Resposta: ${autor}`)
+
+let livro
+livro = prompt("Qual seu livro favorito?")
+console.log(`Resposta: ${livro}`)
+
+let game
+game = prompt("Qual seu jogo de videogame favorito?")
+console.log(`Resposta: ${game}`)
+
+let bebida
+bebida = prompt("Qual sua bebida favorita?")
+console.log(`Resposta: ${bebida}`)*/
+
+// Exercício de escrita de código 3 - a)
+
+ /*let comidaPreferida=['batata frita', 'salada', 'pizza', 'purê de batata', 'batata em conserva'] 
+ console.log(comidaPreferida)
+
+// Exercício de escrita de código 3 - b)
+
+console.log(`Essas são minhas comidas preferidas, 
+${comidaPreferida[0]}
+${comidaPreferida[1]}
+${comidaPreferida[2]}
+${comidaPreferida[3]}
+${comidaPreferida[4]}`)
+
+// Exercício de escrita de código 3 - c)
+
+comidaPreferida[1]=prompt("Qual sua comida favorita?")
+console.log(comidaPreferida)*/
+
+// Exercício de escrita de código 4
+
+let perguntas=["Você já almoçou hoje?", "Bebeu água hoje?", "Já elogiou alguém hoje?"]
+let respostas = [true, true, false]
+console.log(perguntas[0], respostas[0])
+console.log(perguntas[1], respostas[1])
+console.log(perguntas[2], respostas[2])
