ed84bcaeb1af79029e5649330d6f4a1900c789cf
Joyce Roberta <joycinha0406@gmail.com>
2020-10-06 20:13:08 -0300

    resolução-dos-desafios-2-e-3

--- semana4/planner/planner/index.js
+++ semana4/planner/planner/index.js
@@ -5,11 +5,33 @@ function tarefaDoDia(){
 
         if(minhaTarefa.value === ""){
             alert("Opa! Insira alguma coisa!!!")
+            return
         }
 
-    minhaLista.innerHTML += `<li>${minhaTarefa.value}</li>`
+    
+    minhaLista.innerHTML += `<li class="limpar" onclick="riscarTarefa(this)">${minhaTarefa.value}</li>`
+
     minhaTarefa.value = ""
 
     console.log(diaSelecionado)
 
 }
+
+function riscarTarefa(id) {
+    id.style.textDecoration = "line-through";
+}
+
+
+
+function zerarTarefas() {
+    let limpar = confirm("Deseja realmente limpar estas tarefas?")
+        if(limpar === true){
+           const limparTarefa = document.getElementsByClassName("limpar");
+           console.log(limparTarefa);
+           for (let i = 0; i < limparTarefa.length; i++) {
+               limparTarefa[i].innerHTML = " "
+           }
+        }
+}
+
+
