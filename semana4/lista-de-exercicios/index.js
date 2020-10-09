7d2b14129fb992dac2058faad98a3e15df124717
Joyce Roberta <joycinha0406@gmail.com>
2020-10-09 11:54:01 -0300

    resolução-do-exercicio-de-leitura-de-codigo-1

--- semana4/lista-de-exercicios/index.js
+++ semana4/lista-de-exercicios/index.js
@@ -1,1 +1,3 @@
 // ------------------------------------Exercícios de interpretação de código 1 
+
+// O código faz uma conversão de valores, através de uma função, com um parâmetro chamado valor em dolar, a função irá captar um valor através do prompt na const cotação, que dará um retorno de o cálculo do valor em dólar que está sendo informação e a cotação que será informada pelo usuário. Na const meuDinheiro é chamada a função indicando qual é o valor do seu parâmetro e então realiza o calculo chamado no return. O resultado apresentado no console, será o da conta valorEmDolar (100) * cotacao (5,60) = R$ 560.
