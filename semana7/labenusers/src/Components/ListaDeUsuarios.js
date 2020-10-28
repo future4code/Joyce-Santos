4da8047f9edc7b8ddaeb01ee8bc76a27dc2bd28e
Joyce Roberta <joycinha0406@gmail.com>
2020-10-27 21:04:48 -0300

    inclusão-do-botão-deletar-usuários

--- semana7/labenusers/src/Components/ListaDeUsuarios.js
+++ semana7/labenusers/src/Components/ListaDeUsuarios.js
@@ -1,6 +1,12 @@
 import React from "react";
-import Styled from "styled-components";
+import styled from "styled-components";
 import axios from "axios";
+
+const Botao = styled.button`
+background-color: magenta;
+margin-top: 5px;
+margin-left: 5px;
+`
 
 
 class ListaDeUsuarios extends React.Component {
@@ -36,11 +42,36 @@ class ListaDeUsuarios extends React.Component {
 
     }
 
+    deletaUsuario = (id) => {
+        axios.delete(
+        `https://us-central1-labenu-apis.cloudfunctions.net/labenusers/users/${id}`,
+        {
+          headers:{
+            Authorization: "joyce-santos-dumont"
+          },
+
+        }
+      )
+      .then(() =>{
+        alert("Tchau Usuário!!!")
+      })
+      .catch((error)  => {
+        alert("Deu ruim aqui também!")
+
+      })
+
+    }
+
   render() {
     const randerizaUsuario = this.state.usuarios.map((usuario) =>{
-      return <p key={usuario.id}> {usuario.name} </p>
-
-    })
+      return (
+        <p key={usuario.id}> 
+          {usuario.name} 
+          <Botao onClick={()=>{this.deletaUsuario(usuario.id)}}>Del Usuário</Botao>
+        </p>
+        )}
+      )
+    
     return (
       <div>
         <h1>Lista de Usuários Cadastrados</h1>
