84e8cd973213dae6eddb4f9d2b6d1f9bb522a89d
joyceroberta <joycinha0406@gmail.com>
2021-01-13 16:54:37 -0300

    Exercicios do dia incompletos até o 3

---
+++ semana16/aula46/src/index.ts
@@ -0,0 +1,74 @@
+import knex from "knex";
+import express, { Request, Response } from "express";
+import dotenv from "dotenv";
+import { AddressInfo } from "net";
+
+/**************************************************************/
+
+dotenv.config();
+
+/**************************************************************/
+
+const connection = knex({   
+  client: "mysql",
+  connection: {
+    host: process.env.DB_HOST,
+    port: 3306,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+  },
+});
+
+/**************************************************************/
+
+const app = express();
+
+app.use(express.json());
+
+const server = app.listen(process.env.PORT || 3003, () => {
+  if (server) {
+    const address = server.address() as AddressInfo;
+    console.log(`Server is running in http://localhost:${address.port}`);
+  } else {
+    console.error(`Failure upon starting server.`);
+  }
+});
+
+/**************************************************************/
+
+app.get('/', testEndpoint)
+
+async function testEndpoint(req:Request, res:Response): Promise<void>{
+  try {
+    const result = await connection.raw(`
+      SELECT * FROM Actor
+    `)
+
+    res.status(200).send(result)
+  } catch (error) {
+    res.status(400).send(error.message)
+  }
+}
+
+/**************************************************************/
+const updateSalary = async (id: string, salary: number): Promise<any> => {
+  await connection("Actor")
+    .update({
+      salary: salary,
+    })
+    .where("id", id);
+};
+
+app.put('/actors/salary/:id', async (req:Request, res:Response) => {
+  try {
+    const update = await updateSalary((req.params.id), req.body.salary);
+  
+    res.status(200).send("Salário atualizado!")
+  } catch (error) {
+    res.status(400).send(error.message)
+  }
+})
+
+
+
