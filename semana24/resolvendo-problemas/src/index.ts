b052dc86de2f45d89ec9902869ce67b2223d2ebc
joyceroberta <joycinha0406@gmail.com>
2021-03-15 17:05:13 -0300

    Exercicio 1

---
+++ semana24/resolvendo-problemas/src/index.ts
@@ -0,0 +1,17 @@
+function isOneEdit(stringA: string, stringB: string): boolean{
+    if(Math.abs(stringB.length - stringA.length) > 1 ) return false
+
+    if(stringA.length > stringB.length) return stringA.includes(stringB)
+    if(stringB.length > stringA.length) return stringB.includes(stringA)
+
+    let charsDiffCount = 0
+
+    for(let i = 0; i < stringA.length; i++){
+        if(stringA[i] !== stringB[i]) charsDiffCount++
+    }
+
+    return charsDiffCount === 1
+    
+}
+
+console.log(isOneEdit("banana", "panana"))
